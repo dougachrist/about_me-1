87368afc0047b1f5525c16a20a970b2595a212e3
‘Fiona0516git config --global user.email ‘fchang16@gmail.com <fchang16@gmaill.comgit congif --global user.name Fiona0516git config --global user.email fchang16@gmaill.com>
2016-06-14 15:21:43 -0700

    second question finished

--- app.js
+++ app.js
@@ -3,9 +3,15 @@
 var userName = prompt('Greetings!!! What is your name?');
 alert('Welcome ' + userName + ', glad you\'re here!');
 
-var answer1 = prompt('Does Fiona have 1 cat?').toLowerCase();
-if(answer1/*.toLowerCase()*/=== 'yes'|| answer1==='y'){
-  alert('Right!');
-}else{
-  alert('Wrong, Fiona actually does not have a pet.');
+var answer1 = prompt('Does Fiona have 2 cats?').toLowerCase();
+if (answer1 === 'yes' || answer1 === 'y') {
+  alert('Wrong! Fiona actually does not have a pet!');
+} else {
+  alert('Right, she does not have a cat.');
+}
+var answer2 = prompt('Does Fiona speak Spanish?').toLowerCase();
+if (answer2 === 'yes' || answer2 === 'y') {
+  alert('Correct! She has been learning Spanish for a year!');
+} else{
+  alert('Wrong, ella hablas Espanol.');
 }
