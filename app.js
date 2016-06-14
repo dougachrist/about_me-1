98998e96e38a445d882cddd9261488f177409b1d
‘Fiona0516git config --global user.email ‘fchang16@gmail.com <fchang16@gmaill.comgit congif --global user.name Fiona0516git config --global user.email fchang16@gmaill.com>
2016-06-14 14:54:24 -0700

    first question finished

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-  alert('yeeaaaa!!')
+'use strict';
+
+var userName = prompt('Greetings!!! What is your name?');
+alert('Welcome ' + userName + ', glad you\'re here!');
+
+var answer1 = prompt('Does Fiona have 1 cat?').toLowerCase();
+if(answer1/*.toLowerCase()*/=== 'yes'|| answer1==='y'){
+  alert('Right!');
+}else{
+  alert('Wrong, Fiona actually does not have a pet.');
+}
