526a1389a7c1bd03f1ae078ea3dedee9abe6022c
dougachrist <dougachrist@gmail.com>
2016-06-16 14:06:14 -0700

    update clean whitespace

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ var answer2 = prompt('Does Fiona speak Spanish?').toLowerCase();
 if (answer2 === 'yes' || answer2 === 'y') {
   alert('Correct! She has been learning Spanish for a year!');
   tallyCorrect++;
-} else{
+} else {
   alert('Wrong, ella habla Espanol.');
 }
 var answer3 = prompt('Can she read people\'s mind?').toLowerCase();
 if (answer3 === 'yes' || answer3 === 'y') {
   alert('Nehhh... She studies pscyhology but she has no idea what you are thinking about.');
-} else{
+} else {
   alert('True, but she would want to know how to do that.');
   tallyCorrect++;;
 }
@@ -30,34 +30,33 @@ var answer4 = prompt('Has she ever donated blood?').toLowerCase();
 if (answer4 === 'yes' || answer4 === 'y') {
   alert('Right, and guess what happened? She fainted afterwards...');
   tallyCorrect++;
-} else{
+} else {
   alert('Wrong! She donated for the first time on her 22nd birthday!');
 }
 var answer5 = prompt('Does she have a sibiling?').toLowerCase();
 if (answer5 === 'yes' || answer5 === 'y') {
   alert('Correct! She has a sister who is working in Shanghai.');
   tallyCorrect++;
-} else{
+} else {
   alert('Wrong! She has a sister that is 4 years older than her.');
 }
 
 var answer6 = parseInt(prompt('What is her lucky number?'));
 var i = 0;
-while (answer6 !== 6 && i < 4){
-  if (answer6 < 6){
+while(answer6 !== 6 && i < 4) {
+  if(answer6 < 6) {
     alert ('Too low!');
   }
-  else if (answer6 > 6){
+  else if(answer6 > 6) {
     alert('Too high!');
   }
   i++;
   answer6 = parseInt(prompt('What is her lucky number?'));
 }
-if (i >= 4 ){
+if(i >= 4 ) {
   alert('Too many tries.');
-
 }
-if (answer6 === 6){
+if(answer6 === 6) {
   alert('Good guess!');
   tallyCorrect++;
 }
@@ -65,7 +64,7 @@ var possibleAns = ['Palm Springs','Las Vegas','Tijuana','Zoo','Home','Santa Moni
 var i = 0;
 while(i < 6) {
   var answer7 = prompt('Where did she celebrate her 22nd birthday?');
-  for(var j = 0; j < possibleAns.length; j++) {
+  for( var j = 0; j < possibleAns.length; j++) {
     if(answer7 === possibleAns[j]) {
       alert('Thats right!');
       tallyCorrect++;
@@ -81,19 +80,19 @@ while(i < 6) {
 var correctAns = 0;
 var i = 0;
 for(i = 0; i <= 7; i++ ) {
-  if (answer1.toLowerCase === 'yes' || answer1 === 'y') {
+  if(answer1.toLowerCase === 'yes' || answer1 === 'y') {
     correctAns = correctAns + 1;}
-  if (answer2.toLowerCase === 'yes' || answer1 === 'y') {
+  if(answer2.toLowerCase === 'yes' || answer1 === 'y') {
     correctAns = correctAns + 1;}
-  if (answer3.toLowerCase === 'yes' || answer1 === 'y') {
+  if(answer3.toLowerCase === 'yes' || answer1 === 'y') {
     correctAns = correctAns + 1;}
-  if (answer4.toLowerCase === 'yes' || answer1 === 'y') {
+  if(answer4.toLowerCase === 'yes' || answer1 === 'y') {
     correctAns = correctAns + 1;}
-  if (answer5.toLowerCase === 'yes' || answer1 === 'y') {
+  if(answer5.toLowerCase === 'yes' || answer1 === 'y') {
     correctAns = correctAns + 1;}
-  if (answer6 === 6){
+  if(answer6 === 6){
     correctAns = correctAns + 1;}
-  if (answer7 === possibleAns[j]) {
+  if(answer7 === possibleAns[j]) {
     correctAns = correctAns + 1;}
   break;
   i++;
